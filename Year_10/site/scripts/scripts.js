9bbee818c1bbaa012d51d1b50d7a29ba374b35f9
Paul Fooks <paulfooks@mac.com>
2018-12-09 11:04:37 +0000

    Year 10
    Modification to scripts. Ternary operator added to hours and minutes

--- Year_10/site/scripts/scripts.js
+++ Year_10/site/scripts/scripts.js
@@ -8,9 +8,11 @@ function dateTimeInsert() {
 
 	// ternary operator if < 10 then add 0 else display date
 	var day = d.getDate() < 10?"0"+d.getDate():d.getDate();
+	var hours = d.getHours() < 10?"0"+d.getHours():d.getHours();
+	var minutes = d.getMinutes() < 10?"0"+d.getMinutes():d.getMinutes();
 	
 	// set date_time to the day, hyphen month + 1, another hyphen and then full year
-    var date_time = day + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+    var date_time = day + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " + hours + ":" + minutes;
     
     // llo at document and get element id - "date_time". Change its innerHTML to var date_time
 	document.getElementById("date_time").innerHTML = date_time;
